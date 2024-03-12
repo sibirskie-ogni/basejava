520f42a57dc25e7ea66257a26461335b04bd8052
Nick Grisha <nik@mail.ru>
2024-03-12 13:19:44 +0300

    created update changed save

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -14,7 +14,29 @@ public class ArrayStorage {
     }
 
     void save(Resume resume) {
-        storage[size++] = resume;
+        if(size<=storage.length){
+        if(size==0){
+            storage[size++]=resume;
+        }else
+        for (int i = 0; i < size; i++) {
+            if(storage[i].uuid == resume.uuid){
+                System.out.println("ERROR: resume with this id have been already created");
+                break;
+            }else if(i==size-1){
+                storage[size++]=resume;
+                break;
+            }
+        }
+        }else System.out.println("ERROR: to many resumes in database");
+    }
+
+    void update(Resume resume){
+        for (int i = 0; i < size; i++) {
+            if (storage[i].uuid.equals(resume.uuid)) {
+                storage[i]=resume;
+                break;
+            }
+        }
     }
 
     Resume get(String uuid) {
@@ -22,14 +44,16 @@ public class ArrayStorage {
             if (storage[i].uuid.equals(uuid)) {
                 return storage[i];
             }
-        }
+        }System.out.println("ERROR: resume with id "+uuid+" haven't been created");
         return null;
     }
 
     void delete(String uuid) {
+        
         for (int i = 0; i < size; i++) {
             if (storage[i].toString().equals(uuid)) {
                 storage[i] = storage[size - 1];
+                storage[size-1]=null;
                 size--;
                 break;
             }
--- src/MainTestArrayStorage.java
+++ src/MainTestArrayStorage.java
@@ -11,10 +11,16 @@ public class MainTestArrayStorage {
         r2.uuid = "uuid2";
         Resume r3 = new Resume();
         r3.uuid = "uuid3";
+        Resume r4 = new Resume();
+        r4.uuid = "uuid4";
+        Resume r5 = new Resume();
+        r5.uuid = "uuid4";
 
         ARRAY_STORAGE.save(r1);
         ARRAY_STORAGE.save(r2);
         ARRAY_STORAGE.save(r3);
+        ARRAY_STORAGE.save(r4);
+        ARRAY_STORAGE.update(r5);
 
         System.out.println("Get r1: " + ARRAY_STORAGE.get(r1.uuid));
         System.out.println("Size: " + ARRAY_STORAGE.size());
